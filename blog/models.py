7c61422f66cd1913cfe9ba0bfea76bb5dd623dbe
Jungwon Kim <garden0329@gmail.com>
2025-04-10 14:13:45 +0900

    Django normal structure + blog APP + Post/Topic model

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    topic = models.ManyToManyField(Topic, related_name='posts')
+
+    def __str__(self):
+        return self.title
+    
+# Create your models here.
